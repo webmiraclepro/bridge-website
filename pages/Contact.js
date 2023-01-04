882fdb48c4bc3583929c7f8b0622288c8ccf23a4
venustar1228 <venustar-1228@outlook.com>
2023-01-04 11:57:20 -0500

    Email done

--- pages/Contact.js
+++ pages/Contact.js
@@ -5,32 +5,32 @@ import Footer from './components/Footer'
 import {useState} from 'react'
 
 function Contact() {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
+  // const [values, setValues] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: "",
+  // });
 
-  const {name, email, message} = values;
+  // const {name, email, message} = values;
 
-  const handleChange = e => setValues(
-    { ...values, [e.target.name]: e.target.name}
-    );
-  const handleSubmit = async e => {
-    e.preventDefault();
-    try{
-      await fetch('http://localhost:3000/api/contact', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      });
-    }
-      catch (err) {
-        console.log(err);
-      }
-  }
+  // const handleChange = e => setValues(
+  //   { ...values, [e.target.name]: e.target.name}
+  // );
+  // const handleSubmit = async e => {
+  //   e.preventDefault();
+  //   try{
+  //     await fetch('http://localhost:3000/api/contact', {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(values),
+  //     });
+  //   }
+  //     catch (err) {
+  //       console.log(err);
+  //     }
+  // }
   return (
     <div>
       <div className="w-full h-[1110px] bg-[url('../public/image/Backgrouns-04.jpg')] bg-cover flex-col">
@@ -54,44 +54,41 @@ function Contact() {
                   </div>
               </div>
               <div className='flex-col items-start justify-center ml-[130px] mt-[130px]'>
-                <form onSubmit={handleSubmit}>
-                  <div className='flex-col ml-[50px]'>
+                <div className='flex-col ml-[50px]'>
+                  <form action='https://formspree.io/f/xpzezdrp' method='post'>
                     <div className=' text-black'>
-                      <input placeholder='Name' name='name' value={name}
+                      <input placeholder='Name' name='Name' 
                       className='w-[700px] h-[30px] rounded-full pl-[15px]'/>
                     </div>
                     <div className='mt-[15px]'>
-                      <input placeholder='Email' name='email' value={email}
+                      <input placeholder='Email' name='Email'
                       className='w-[700px] h-[30px] rounded-full pl-[15px]'/>
                     </div>
                     <div className='flex'>
                       <div className='mt-[15px]'>
-                        <input placeholder='I am a ...' 
+                        <input placeholder='I am a ...' name='Job'
                         className='w-[340px] h-[30px] rounded-full pl-[15px]'/>
                       </div>
                       <div className='mt-[15px] ml-[20px]'>
-                        <input placeholder='Phone' name='message' value={message}
-                        onChange={handleChange}
+                        <input placeholder='Phone' name='Phone'
                         className='w-[340px] h-[30px] rounded-full pl-[15px]'/>
                       </div>
                     </div>
                     <div className='mt-[15px]'>
-                      <input  
-                      className='w-[700px] h-[300px] rounded-[20px] pl-[15px]'/>
+                      <input name='Message' className='w-[700px] h-[300px] rounded-[20px] pl-[15px]'/>
                     </div>
                     <div className='w-[300px] h-[100px] rounded-full flex items-center'>     
-                      <button className='py-[7px] px-[20px] bg-[#42EBC8] rounded-full font-bold'
-                            onClick={() => router.push('/Contact')}>
-                            SUBMIT
-                        </button>
+                      <button type='submit' className='py-[7px] px-[20px] bg-[#42EBC8] rounded-full font-bold'>
+                        SUBMIT
+                      </button>
                     </div>
                     <div className='w-[400px] h-[400px] ml-[500px] overflow-hidden rounded-full'>
                         <div className='w-[400px] h-[300px] overflow-hidden'>
                             <Image src='/image/Space_whales_NFT.jpg' width={400} height={400} className='rounded-full'/>
                         </div>
                     </div>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
           </div>
         </div>
